011a2da2d26766e456d2b2ce050ed9697aec72eb
codearct <codearct@gmail.com>
2025-11-28 23:01:56 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+/**
+ * @copyright 2025 Mehmet Akif YILDIZ
+ * @license Apache-2.0
+ */
+
+import { Button } from "@/components/ui/button"
+
+export const App =()=>{
+  return <Button>Click Me</Button>;
+};
---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+/**
+ * Node modules
+ */
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+/**
+ * Styles
+ */
+import '@/index.css';
+
+/**
+ * ComponentsF
+ */
+import { App } from '@/App.tsx';
+import { Sidebar } from '@/components/Sidebar';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div className='min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10'>
+      <Sidebar />
+      <App />
+    </div>
+  </StrictMode>,
+);
