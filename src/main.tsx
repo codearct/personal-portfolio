38e136dc549466909c9233d16f993b10ab37ea77
codearct <codearct@gmail.com>
2025-11-29 12:58:03 +0300

    add FloatingMenu component and integrate it into main layout

--- src/main.tsx
+++ src/main.tsx
@@ -14,11 +14,13 @@ import '@/index.css';
  */
 import { App } from '@/App.tsx';
 import { Sidebar } from '@/components/Sidebar';
+import { FloatingMenu } from '@/components/FloatingMenu';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className='min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10'>
       <Sidebar />
+      <FloatingMenu />
       <App />
     </div>
   </StrictMode>,
