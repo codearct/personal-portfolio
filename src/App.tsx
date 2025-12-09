6f53444e8c0ee811d7e7317d8488ba93840e1e85
codearct <codearct@gmail.com>
2025-12-09 15:50:17 +0300

    feat: add Lenis for smooth scrolling effect in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@
  * @copyright 2025 Mehmet Akif YILDIZ
  * @license Apache-2.0
  */
+/**
+ * Noode Modules
+ */
+import Lenis from 'lenis';
+import { useEffect } from 'react';
 
 /**
  * Components
@@ -16,6 +21,17 @@ import { Testimonials } from '@/components/Testimonials';
 import { Contact } from '@/components/Contact';
 
 export const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main className='flex flex-col container mx-auto p-10 max-w-4xl lg:pr-10 lg:pl-0 lg:max-w-6xl'>
       <Hero />
